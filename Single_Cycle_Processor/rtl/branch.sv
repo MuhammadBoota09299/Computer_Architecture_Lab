a29c721f1656afded4473154ab59b736e5c6d824
Muhammad Boota <https://github.com/MuhammadBoota09299/Computer_Architecture_Lab.git>
2025-02-18 01:17:45 +0500

    branch type done

--- Single_Cycle_Processor/rtl/branch.sv
+++ Single_Cycle_Processor/rtl/branch.sv
@@ -1,3 +1,4 @@
+import packages::*;
 module branch (
     input logic [31:0]rdata1,rdata2,
     input logic [2:0]br_type,
@@ -5,12 +6,12 @@ module branch (
 );
     always_comb begin
         case (br_type)
-            BEQ : br_taken = (raddr1 == raddr2) ? 2'b1 : 2'b0;
-            BNE : br_taken = (raddr1 != raddr2) ? 2'b1 : 2'b0;
-            BLT : br_taken = ($signed(raddr1) < $signed(raddr2)) ? 1'b1 : 1'b0;
-            BGE : br_taken = ($signed(raddr1) >= $signed(raddr2)) ? 1'b1 : 1'b0;
-            BLTU: br_taken = (raddr1 < raddr2) ? 1'b1 : 1'b0; 
-            BGEU: br_taken = (raddr1 >= raddr2) ? 1'b1 : 1'b0;
+            BEQ : br_taken = (rdata1 == rdata2) ? 2'b1 : 2'b0;
+            BNE : br_taken = (rdata1 != rdata2) ? 2'b1 : 2'b0;
+            BLT : br_taken = ($signed(rdata1) < $signed(rdata2)) ? 1'b1 : 1'b0;
+            BGE : br_taken = ($signed(rdata1) >= $signed(rdata2)) ? 1'b1 : 1'b0;
+            BLTU: br_taken = (rdata1 < rdata2) ? 1'b1 : 1'b0; 
+            BGEU: br_taken = (rdata1 >= rdata2) ? 1'b1 : 1'b0;
             PC  : br_taken =  1'b0;
             ALU : br_taken =  1'b1;
             default: br_taken=1'b0;
