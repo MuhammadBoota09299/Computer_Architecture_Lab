d5415f9febedf1a39a897948b98d69f9a32a3ea0
Muhammad Boota <https://github.com/MuhammadBoota09299/Computer_Architecture_Lab.git>
2025-02-17 23:12:24 +0500

    brack time

--- Single_Cycle_Processor/rtl/decoder.sv
+++ Single_Cycle_Processor/rtl/decoder.sv
@@ -1,9 +1,9 @@
 import packages::*;
 module decoder (
     input logic [31:0]instruction,
-    output logic reg_wr,sel_B,mem_wr,
+    output logic reg_wr,sel_B,mem_wr,sel_A,
     output logic [1:0]wb_sel,
-    output logic [2:0]rd_wr_mem,
+    output logic [2:0]rd_wr_mem,br_type,
     output logic [31:0]immediate,
     output logic [3:0]alu_op
 );
@@ -18,24 +18,38 @@ rd_wr_mem=funct3;
             alu_op={funct3,instruction[30]};
             reg_wr=1'b1;
             sel_B=1'b0;
+            sel_A=1'b0;
             mem_wr=1'b0;
             wb_sel=2'b0;
+            br_type=PC;
         end 
         I_TYPE || LOAD:begin
             alu_op=ADD;
             reg_wr=1'b1;
             immediate = { {20{instruction[31]}}, instruction[31:20] };
             sel_B=1'b1;
+            sel_A=1'b0;
             wb_sel = (opcode == LOAD) ? 2'b1 : 2'b0;
             mem_wr=1'b0;
+            br_type=PC;
         end
         STORE:begin
             alu_op=ADD;
             reg_wr=1'b0;
             mem_wr=1'b1;
             sel_B=1'b1;
-            wb_sel=2'b0;
+            sel_A=1'b0;
             immediate={{20{instruction[31]}},instruction[31:25], instruction[11:7]};
+            br_type=PC;
+        end
+        BRANCH:begin
+            br_type=funct3;
+            alu_op=ADD;
+            reg_wr=1'b0;
+            sel_A=1'b1;
+            sel_B=1'b1;
+            mem_wr=1'b0;
+
         end
         default:begin
             alu_op=4'b0;
