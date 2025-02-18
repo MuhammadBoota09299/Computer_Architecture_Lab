104073f8c1348ac6f12da32ef7bd92f9c478abb2
Muhammad Boota <https://github.com/MuhammadBoota09299/Computer_Architecture_Lab.git>
2025-02-18 16:50:47 +0500

    single cycle is completed

--- Single_Cycle_Processor/rtl/instruction_memory.sv
+++ Single_Cycle_Processor/rtl/instruction_memory.sv
@@ -8,17 +8,6 @@ module instruction_memory (
     end
 
     initial begin
-        inst_memory[0]=32'b0;
-        inst_memory[1]=32'h05400413;
-        inst_memory[2]=32'h03c00493;
-        inst_memory[3]=32'h00940c63;
-        inst_memory[4]=32'h00944663;
-        inst_memory[5]=32'h40940433;
-        inst_memory[6]=32'hff5ff06f;
-        inst_memory[7]=32'h408484b3;
-        inst_memory[8]=32'hfedff06f;
-        inst_memory[9]=32'h00802423;
-        inst_memory[10]=32'h00802503;
-        inst_memory[11]=32'h0000006f;
+    $readmemh("/home/aziz/Documents/Computer_Architecture_Lab/Single_Cycle_Processor/commands_c/build/main.txt",inst_memory);
     end
 endmodule
