35a6b6110a4bfdd28b762d361ff79bb6024986e5
Muhammad Boota <boota09299@gmail.com>
2025-03-01 21:22:59 +0500

    rtl of pipeline is almost done its time to remove errors

--- Single_Cycle_Processor/rtl/pipline_reg.sv
+++ Single_Cycle_Processor/rtl/pipline_reg.sv
@@ -1,11 +1,11 @@
 //fetch to decode and execute phase
 module pipline_fetch_to_decode (
-    input clock,reset,
+    input clock,reset,stall,br_taken,
     input logic [31:0]pc_fetch,instruction_fetch,
     output logic [31:0]pc_execute,instruction_execute;
 );
     always_ff @( posedge clock,posedge reset ) begin 
-        if (reset) begin
+        if (reset | stall | br_taken) begin
             pc_execute<=32'b0;
             instruction_execute<=32'b00000013;
         end
@@ -19,10 +19,10 @@ endmodule
 //execute to memory and writeback phase
 module pipline_execute_to_memory (
     input logic [31:0]pc_execute, alu_execute,mem_wdata_execute,instruction_execute
-    input logic waddr_execute,reg_wr_execute,mem_wr_execute,clock,reset,
+    input logic reg_wr_execute,mem_wr_execute,clock,reset,wb_sel_execute,
     input logic [2:0]rd_wr_mem_execute,
     output logic [31:0]pc_mem, alu_mem,mem_wdata_mem,instruction_mem
-    output logic waddr_mem,reg_wr_mem,mem_wr_mem,
+    output logic waddr_mem,mem_wr_mem,wb_sel_mem,
     output logic [2:0]rd_wr_mem_mem,    
 );
     always_ff @( posedge clock ) begin 
@@ -32,7 +32,7 @@ module pipline_execute_to_memory (
             mem_wdata_mem <=32'b0;
             instruction_mem <=32'h00000013;
             waddr_mem <=1'b0;
-            reg_wr_mem <=1'b0;
+            wb_sel_mem <=1'b0;
             mem_wr_mem <=1'b0;
             rd_wr_mem_mem <=3'b0;
         end
@@ -41,9 +41,8 @@ module pipline_execute_to_memory (
             alu_mem <= alu_execute;
             mem_wdata_mem <= mem_wdata_execute;
             instruction_mem <= instruction_execute;
-            waddr_mem <= waddr_execute;
-            reg_wr_mem <=reg_wr_execute;
             mem_wr_mem <=mem_wr_execute;
+            wb_sel_mem <= wb_sel_execute;
             rd_wr_mem_mem <=rd_wr_mem_execute;
         end
     end
--- Single_Cycle_Processor/rtl/single_cycle_processor.sv
+++ Single_Cycle_Processor/rtl/single_cycle_processor.sv
@@ -1,7 +1,7 @@
 module single_cycle_processor (
     input logic reset,clock
 );
-logic [31:0]inst_decode,inst,instruction,pc,pc_decode,pc_next,result,rdata1,rdata2,rs1,rs2,immediate,mem_rdata,reg_data,pc_alu;
+logic [31:0]inst,inst,instruction,pc,pc_decode,pc_next,result,rdata1,rdata2,rs1,rs2,immediate,rdata_mem,reg_data,pc_in;
 logic [3:0]alu_op;
 logic [2:0]rd_wr_mem,br_type;
 logic [1:0]wb_sel;
@@ -10,24 +10,24 @@ logic reg_wr,sel_B,sel_A,mem_wr,br_taken;
 //-----------fetch phase----------------
 
 //pc
-pc _pc(.clock,.reset,.pc_in(pc_alu),.pc);
+pc _pc(.clock,.reset,.pc_in,.pc);
 pc_add pc_4_add(.pc,.pc_next);
 // PC or ALU slector mux
-mux2_1 PC_ALU_sel(.sel(br_taken),.input0(pc_next),.input1(result),.out(pc_alu));
+mux2_1 PC_ALU_sel(.sel(br_taken),.input0(pc_next),.input1(result),.out(pc_in));
 //instruction memory
 instruction_memory _instruction_memory(.addr(pc),.inst);
 //pipeline register
-pipline_fetch_to_decode fetch_to_decode_reg(.clock,.reset,.pc_fetch(pc),.instruction_fetch(inst),
-.pc_execute,.instruction_execute(instruction))
+pipline_fetch_to_decode fetch_to_decode_reg(.clock,.reset,.pc_fetch(pc),.instruction_fetch(inst),.stall,.br_taken,
+.pc_execute,.instruction_execute(instruction));
 
 
 //--------------decode and execute phase-------------
 
 //registerfile
 registerfile _registerfile(.raddr1(instruction[19:15]),.raddr2(instruction[24:20]),
-.waddr,.wdata(reg_data),.reg_wr(reg_wr_mem),.rdata1,.rdata2,.clock,.reset);
+.waddr(instruction_mem[11:7]),.wdata(reg_data),.reg_wr(reg_wr_mem),.rdata1,.rdata2,.clock,.reset);
 //decoder
-decoder _decoder(.instruction,.reg_wr,.alu_op,.immediate,.sel_B,.sel_A,.mem_wr,.wb_sel,.rd_wr_mem,.br_type);
+decoder _decoder(.instruction,.reg_wr,.alu_op,.immediate,.sel_B,.sel_A,.mem_wr,.wb_sel,.rd_wr_mem,.br_type,.stall);
 //sel_A mux for rs1 or pc
 mux2_1 Sel_A(.input0(rdata1),.input1(pc_execute),.out(rs1),.sel(sel_A));
 //sel_B mux for rs2 or immediate
@@ -40,13 +40,15 @@ branch branch_condition(.rdata1,.rdata2,.br_type,.br_taken);
 
 //---------------memory and writeback phase---------------
 
-pipline_execute_to_memory execute_to_memory_reg(.clock,.reset,.pc_execute,.alu_execute(result),
-.mem_wdata_execute(raddr2),.instruction_execute(instruction), .waddr_execute(instruction[11:7]),
-.reg_wr_execute(reg_wr),.mem_wr_execute(mem_wr),.rd_wr_mem_execute(rd_wr_mem),
-.pc_mem, .alu_mem,.mem_wdata_mem(wdata_mem),.instruction_mem, .waddr_mem(waddr),.reg_wr_mem,.mem_wr_mem, rd_wr_mem_mem);
+//pipline registor
+pipline_execute_to_memory execute_to_memory_reg(.clock,.reset,.pc_execute,.alu_execute(result),.mem_wdata_execute(raddr2),
+.instruction_execute(instruction),.reg_wr_execute(reg_wr),.mem_wr_execute(mem_wr),.rd_wr_mem_execute(rd_wr_mem),.wb_sel_execute(wb_sel),
+.pc_mem, .alu_mem(addr_mem),.mem_wdata_mem(wdata_mem),.instruction_mem,.mem_wr_mem, rd_wr_mem_mem,reg_wr_mem,.wb_sel_mem);
+//add 4 in pc of memory phase
+pc_add pc_mem_4_add(.pc(pc_mem),.pc_next(pc_next_mem));
 //data memory
-data_memory Data_memory(.clock,.reset,.addr_mem(alu_mem>>1),.wdata_mem,.rdata(mem_rdata),.mem_wr(mem_wr_mem),.rd_wr_mem(rd_wr_mem_mem));
+data_memory Data_memory(.clock,.reset,.addr_mem,.wdata_mem,.rdata_mem,.mem_wr(mem_wr_mem),.rd_wr_mem(rd_wr_mem_mem));
 //writeback mux
-mux3_1 writeback_mux(.input0(result),.input1(mem_rdata),.input2(pc_next),.out(reg_data),.sel(wb_sel));
+mux3_1 writeback_mux(.input0(result),.input1(rdata_mem),.input2(pc_next_mem),.out(reg_data),.sel(wb_sel_mem));
 
 endmodule
