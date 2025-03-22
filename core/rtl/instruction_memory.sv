050ff492cb9b8e35e28705912b4c557dc3967c55
Muhammad Boota <boota09299@gmail.com>
2025-03-22 12:41:40 +0500

    all files has been orginized

---
+++ core/rtl/instruction_memory.sv
@@ -0,0 +1,14 @@
+module instruction_memory (
+    input logic [31:0]addr,
+    output logic [31:0]inst
+);
+    logic [31:0]inst_memory [31:0]='{default:0};
+    always_comb begin
+        inst=inst_memory[addr>>2];
+    end
+    
+    initial begin
+    //$readmemh("/home/aziz/Documents/Computer_Architecture_Lab/assembly/c_commands/build/main.txt",inst_memory);
+    $readmemh("/home/aziz/Documents/Computer_Architecture_Lab/assembly/riscv_commands/build//gcd.txt",inst_memory);
+    end
+endmodule
